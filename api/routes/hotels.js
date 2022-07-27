cbf770f5a83fca150680da263a27591c546fa158
Ali Abbani <aliabbani08@gmail.com>
2022-07-28 00:18:06 +0300

    up

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -26,9 +26,11 @@ router.post("/", async (req, res) => {
 router.put("/:id", async (req, res) => {
   try {
     // set is a mongodb method
-    const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, {
-      $set: req.body,
-    });
+    const updatedHotel = await Hotel.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { $new: true }
+    );
     res.status(200).json(updatedHotel);
   } catch (err) {
     res.status(500).json(err);
